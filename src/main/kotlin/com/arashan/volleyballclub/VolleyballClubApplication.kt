45d083efa0211ca0c8b637004c44ec9808898c27
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-09-16 11:48:13 +0600

    Initial commit

---
+++ src/main/kotlin/com/arashan/volleyballclub/VolleyballClubApplication.kt
@@ -0,0 +1,11 @@
+package com.arashan.volleyballclub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VolleyballClubApplication
+
+fun main(args: Array<String>) {
+    runApplication<VolleyballClubApplication>(*args)
+}
